3cf50acfac84995d15ab29cb7b8fe30a28013db6
Vladas Tamoshaitis <amd.vladas@gmail.com>
2019-11-27 21:18:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,18 @@ import requests
 import json
 import pytest
 
+
+HTTP_OK = 200
+HTTP_BAD_REQUEST = 400
+HTTP_NOT_FOUND = 404
+
+
 def test_auth():
     url = 'http://127.0.0.1:5000/login'
     data = {'login': '222', 'password': '333'}
     response = requests.post(url, json=data)
     response_data = response.json()
     assert response_data['error'] == """Exception('Access denied. This login does not exist')"""
-
+    assert response.status_code == HTTP_NOT_FOUND
 
 
