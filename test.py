aaddce6d3709c8a33f8ecdd9866e18302bf1eed0
Vladas Tamoshaitis <amd.vladas@gmail.com>
2019-11-27 21:11:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ import pytest
 def test_auth():
     url = 'http://127.0.0.1:5000/login'
     data = {'login': '222', 'password': '333'}
-    res = requests.post(url, json=data)
-    res_2 = eval(res.text)
-    assert res_2['error'] == """Exception('Access denied. This login does not exist')"""
+    response = requests.post(url, json=data)
+    response_data = response.json()
+    assert response_data['error'] == """Exception('Access denied. This login does not exist')"""
 
 
 
