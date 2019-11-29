9ac5cb749ec523bf4aa043bffc1c0fb09b20bc43
aquaracer <50531938+aquaracer@users.noreply.github.com>
2019-11-30 00:31:32 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app import controller, models
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 import config
+from const import HTTP_OK, HTTP_BAD_REQUEST, HTTP_NOT_FOUND
 
 app = Flask(__name__)
 app.debug = config.DevelopementConfig.DEBUG
@@ -14,15 +15,15 @@ def hello_world():
 
 @app.route('/users/<int:page_number>/<int:page_size>', methods=['GET'])
 def users_list(page_number:int, page_size:int):
-    result = {'data':'', 'error':''}
-    code = 200
+    result = {}
+    code = HTTP_OK
     try:
         final_list = controller.list_of_users(page_number, page_size)
         result['data'] = final_list
     except Exception as e:
         result['error']=repr(e)
         print(result) # выводим лог в stdout
-        code = 400
+        code = HTTP_BAD_REQUEST
     return jsonify(result), code
 
 
@@ -32,29 +33,30 @@ def user_registration():
     login = json['login']
     password = json['password']
     admin = json['admin']
-    result = {'data': '', 'error': ''}
-    code = 200
+    expiration_date = json['expiration_date']
+    result = {}
+    code = HTTP_OK
     try:
-        controller.registration(login, password, admin) # пытаемся зарегистрировать пользователя
+        controller.registration(login, password, admin, expiration_date) # пытаемся зарегистрировать пользователя
         result['data'] = 'new user has been registered successfully'
     except Exception as e:
         result['error'] = repr(e)
         print(result) # выводим лог в stdout
-        code = 400
+        code = HTTP_BAD_REQUEST
     return jsonify(result), code
 
 
 @app.route('/user/<int:id>', methods=['GET'])
 def get_user_id(id:int):
-    result = {'data': '', 'error': ''}
-    code = 200
+    result = {}
+    code = HTTP_OK
     try:
         login = controller.get_user_info(id)  # пытаемся получить информацию о пользователе по ID
         result['data'] = login
     except Exception as e:
         result['error'] = repr(e)
         print(result)  # выводим лог в stdout
-        code = 400
+        code = HTTP_NOT_FOUND
     return jsonify(result), code
 
 
@@ -62,15 +64,15 @@ def get_user_id(id:int):
 def delete_info():
     json = request.get_json(force=True)
     user_id = json['user_id']
-    result = {'data': '', 'error': ''}
-    code = 200
+    result = {}
+    code = HTTP_OK
     try:
         report = controller.delete_user(user_id)  # пытаемся удалить информацию о пользователе по ID
         result['data'] = report
     except Exception as e:
         result['error'] = repr(e)
         print(result)  # выводим лог в stdout
-        code = 400
+        code = HTTP_BAD_REQUEST
     return jsonify(result), code
 
 
@@ -79,15 +81,15 @@ def update_info():
     json = request.get_json(force=True)
     user_id = json['user_id']
     password = json['password']
-    result = {'data': '', 'error': ''}
-    code = 200
+    result = {}
+    code = HTTP_OK
     try:
         report = controller.update_user(user_id, password)  # пытаемся удалить информацию о пользователе по ID
         result['data'] = report
     except Exception as e:
         result['error'] = repr(e)
         print(result)  # выводим лог в stdout
-        code = 400
+        code = HTTP_BAD_REQUEST
     return jsonify(result), code
 
 
@@ -96,15 +98,15 @@ def user_auth():
     json = request.get_json(force=True)
     login = json['login']
     password = json['password']
-    result = {'data': '', 'error': ''}
-    code = 200
+    result = {}
+    code = HTTP_OK
     try:
         token = controller.auth(login, password)  # пытаемся пройти аутентификацию и получить JWT токен
         result['data'] = str(token)
     except Exception as e:
         result['error'] = repr(e)
         print(result)  # выводим лог в stdout
-        code = 400
+        code = HTTP_NOT_FOUND
     return jsonify(result), code
 
 
--- test.py
+++ test.py
@@ -3,17 +3,60 @@ import json
 import pytest
 
 
-HTTP_OK = 200
-HTTP_BAD_REQUEST = 400
-HTTP_NOT_FOUND = 404
+@pytest.mark.parametrize("page_number, page_size, expected",
+    [('1', '10', True),
+     ('3', '1', True),
+     ('5', '5', False) ])
+def test_users_list(page_number, page_size, expected):
+    url = 'http://localhost:5000/users/{0}/{1}'.format(page_number, page_size)
+    response = requests.get(url)
+    response = response.json()
+    assert bool(response['data']) == expected
 
 
-def test_auth():
-    url = 'http://127.0.0.1:5000/login'
-    data = {'login': '222', 'password': '333'}
+@pytest.mark.parametrize("login_input, password_input, admin_status_input, expiration_date_input, key_input, expected",
+                        [("Kolya", "444", True, "2019/11/29", "data", 'new user has been registered successfully'),
+                         ("Olya333", "7772777", False, "2019/11/29", "error", """Exception('This login is busy. Please create another')"""),
+                         ("Masha", "7772777", "admin", "2019/11/29", "error", """Exception('invalid format of admin_status')"""),
+                         ("Lena", "", True, "2019/11/29", "error", """Exception('invalid format of password. password should contain at least 3 symbols')"""),
+                         ])
+def test_user_registration(login_input, password_input, admin_status_input, expiration_date_input, key_input, expected):
+    url = 'http://localhost:5000/user'
+    data = {'login': login_input, 'password': password_input, "admin": admin_status_input, 'expiration_date' : expiration_date_input}
     response = requests.post(url, json=data)
-    response_data = response.json()
-    assert response_data['error'] == """Exception('Access denied. This login does not exist')"""
-    assert response.status_code == HTTP_NOT_FOUND
+    response = response.json()
+    assert response[key_input] == expected
 
 
+@pytest.mark.parametrize("id_input, type_of_assertion, key_input, expected",
+                         [(1, 'logical', 'data', True),
+                          (32, 'string', 'error', """Exception('There is no such user in database')""")
+                         ])
+def test_get_user_id(id_input, type_of_assertion, key_input, expected):
+    url = 'http://localhost:5000/user/{}'.format(id_input)
+    response = requests.get(url)
+    response = response.json()
+    if type_of_assertion == 'logical':
+        assert bool(response[key_input]) == expected
+    else:
+        assert response[key_input] == expected
+
+
+@pytest.mark.parametrize("login_input, password_input, key_input, expected",
+    [('222', '333', 'error', """Exception('Access denied. This login does not exist')"""),
+     ("Olya333", "7772777", 'data',  "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjpbMV0sImlzX2FkbWluIjpbZmFsc2VdLCJpc3MiOiJmbGFza19hdXRoX2FwcGxpY2F0aW9uIiwiZXhwIjoxNTc3NTc3NjAwfQ.ZqvZ2Zm2DXH_CMzTJkg0xtBqpr15128-l4B8McGdY_I'"),
+     ("Vasya", "111", 'error', """Exception('Access denied. Subscribe expired')""" )                         ])
+def test_auth(login_input, password_input, key_input, expected):
+    url = 'http://localhost:5000/login'
+    data = {'login': login_input, 'password': password_input}
+    response = requests.post(url, json=data)
+    response = response.json()
+    assert response[key_input] == expected
+
+
+
+# def test_delete_info():
+#     pass
+#
+# def test_update_info():
+#     pass
