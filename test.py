28952948240d850e66e9c4061b9f83e74979e99b
Vladas Tamoshaitis <amd.vladas@gmail.com>
2019-12-01 21:48:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,32 +17,41 @@ def connect():
         raise
     finally:
         session.close()
+        
+        
+@pytest.fixture(autouse=True)
+def clean_db():
+    yield
+    with connect() as session:  # очищаем таблицу
+        operation_result = session.query(User).delete()
+        
+        
+@pytest.fixture
+def user(client):
+    def wrapper(data):
+        create_user_url = 'http://localhost:5000/user'
+        response = client.post(create_user_url, data=data)
+        return response.json()
+    return wrapper
+    
 
 
 @pytest.mark.parametrize("page_number, page_size, expected",
     [('1', '10', True),
      ('3', '1', True),
      ('5', '5', False) ])
-def test_users_list(page_number, page_size, expected):
-    url = 'http://localhost:5000/user'
-    data = {"login": "user_1", "password": "111",
-            "admin" : False, "expiration_date" : "2019/12/29"  }
-    response = requests.post(url, json=data)
-    data = {"login": "user_2", "password": "222",
-            "admin": False, "expiration_date": "2019/12/29"}
-    response = requests.post(url, json=data)
-    data = {"login": "user_3", "password": "333",
-            "admin": False, "expiration_date": "2019/12/29"}
-    response = requests.post(url, json=data)
-    data = {"login": "user_4", "password": "444",
-            "admin": False, "expiration_date": "2019/12/29"}
-    response = requests.post(url, json=data) # вносим тестовые данные в таблицу
+def test_users_list(page_number, page_size, expected, user, client):
+    users = [
+        {"login": "user_1", "password": "111", "admin" : False, "expiration_date" : "2019/12/29"},
+        {"login": "user_2", "password": "222", "admin": False, "expiration_date": "2019/12/29"},
+        {"login": "user_3", "password": "333", "admin": False, "expiration_date": "2019/12/29"},
+        {"login": "user_4", "password": "444", "admin": False, "expiration_date": "2019/12/29"}
+    ]
+    [user(user_data) for user_data in users]
 
-    url = 'http://localhost:5000/users/{0}/{1}'.format(page_number, page_size)
-    response = requests.get(url)
+    user_list_url = 'http://localhost:5000/users/{0}/{1}'.format(page_number, page_size)
+    response = client.get(user_list_url)
     response = response.json()
-    with connect() as session:  # очищаем таблицу
-        operation_result = session.query(User).delete()
     assert bool(response['data']) == expected
 
 
@@ -61,54 +70,37 @@ def test_users_list(page_number, page_size, expected):
          """Exception('This login is busy. Please create another')""")
     ]
 )
-def test_user_registration_negative(JSON_input, expected):
-     url = 'http://localhost:5000/user'
-     first_data = {'login': 'user_1', 'password': '111', "admin": False, 'expiration_date' : '2019/12/31'}
-     requests.post(url, json=first_data)
-     response = requests.post(url, json=JSON_input)
-     response = response.json()
-     with connect() as session:  # очищаем таблицу
-         operation_result = session.query(User).delete()
+def test_user_registration_negative(JSON_input, expected, user):
+     user({'login': 'user_1', 'password': '111', "admin": False, 'expiration_date' : '2019/12/31'})
+     response = user(JSON_input)
      assert response['error'] == expected
 
 
-def test_user_registration_positive():
-    url = 'http://localhost:5000/user'
-    data = {'login': 'user_1', 'password': '111', "admin": False, 'expiration_date': '2019/12/31'}
-    response = requests.post(url, json=data)
-    response = response.json()
-    with connect() as session:  # очищаем таблицу
-        operation_result = session.query(User).delete()
+def test_user_registration_positive(user):
+    response = user({'login': 'user_1', 'password': '111', "admin": False, 'expiration_date': '2019/12/31'})
     assert response['data'] == 'new user has been registered successfully'
 
 
-def test_get_user_id_positive ():
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'}
-    requests.post(url, json=first_data)  # регистрируем юзера
+def test_get_user_id_positive (user):
+    user({"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'})
     with connect() as session: # получаем ID по логину
         id = session.query(User.id).filter(User.login == "user_2").first()
-    url = 'http://localhost:5000/user/{}'.format(id[0])
-    response = requests.get(url)
+    get_user_url = 'http://localhost:5000/user/{}'.format(id[0])
+    response = requests.get(get_user_url)
     response = response.json()
-    with connect() as session:
-        operation_result = session.query(User).delete()
     assert response['data'][0] == 'user_2'
 
 
-def test_get_user_id_negative ():
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'}
-    requests.post(url, json=first_data)  # регистрируем юзера
+def test_get_user_id_negative (user):
+    user({"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'})
     with connect() as session: # получаем ID по логину
         id = session.query(User.id).filter(User.login == "user_2").first()
-    id = int(id[0]) + 1
-    url = 'http://localhost:5000/user/{}'.format(id)
-    response = requests.get(url)
+    invalid_id = int(id[0]) + 1
+    get_user_url = 'http://localhost:5000/user/{}'.format(invalid_id)
+    response = requests.get(get_user_url)
     response = response.json()
-    with connect() as session:
-        operation_result = session.query(User).delete()
     assert response['error'] == """Exception('There is no such user in database')"""
+
 
 @pytest.mark.parametrize(
     "json_input, expected",
@@ -119,24 +111,22 @@ def test_get_user_id_negative ():
     ]
 )
 def test_delete_info_negative(json_input, expected):
-    url = 'http://localhost:5000/user'
-    response = requests.delete(url, json=json_input)
+    delete_user_url = 'http://localhost:5000/user'
+    response = requests.delete(delete_user_url, json=json_input)
     response = response.json()
     assert response['error'] == expected
 
-def test_delete_info_positive():
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'}
-    requests.post(url, json=first_data)  # регистрируем юзера
+
+def test_delete_info_positive(user):
+    user({"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'})
     with connect() as session:
         id = session.query(User.id).filter(User.login == "user_2").first()
 
-    url = 'http://localhost:5000/user'
+    delete_user_url = 'http://localhost:5000/user'
     json = {'user_id': id[0]}
-    response = requests.delete(url, json=json)
+    response = requests.delete(delete_user_url, json=json)
     response = response.json()
     assert response['data'] == 'user has been deleted'
-
 
 
 @pytest.mark.parametrize(
@@ -152,24 +142,20 @@ def test_delete_info_positive():
     ]
 )
 def test_update_info_negative(json_input, expected):
-    url = 'http://localhost:5000/user'
-    response = requests.patch(url, json=json_input)
+    update_user_password_url = 'http://localhost:5000/user'
+    response = requests.patch(update_user_password_url, json=json_input)
     response = response.json()
     assert response['error'] == expected
 
 
-def test_update_info_positive():
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'}
-    requests.post(url, json=first_data)  # регистрируем юзера
+def test_update_info_positive(user):
+    user({"login": "user_2", "password": "111", "admin": False, "expiration_date": '2019/12/31'})
     with connect() as session:
         id = session.query(User.id).filter(User.login == "user_2").first()
-    url = 'http://localhost:5000/user'
+    update_user_password_url = 'http://localhost:5000/user'
     data = {"user_id": id[0], "password": '222'}
-    response = requests.patch(url, json=data)
+    response = requests.patch(update_user_password_url, json=data)
     response = response.json()
-    with connect() as session:
-        operation_result = session.query(User).delete()
     assert response['data'] == 'password has been updated'
 
 
@@ -184,30 +170,20 @@ def test_update_info_positive():
         ({'login': 'user_1', 'password': '111'}, """Exception('Access denied. Subscribe expired')""")
     ]
 )
-def test_auth_negative(JSON_input, expected):
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_1", "password": "111", "admin": False, "expiration_date": '2019/11/30'}
-    requests.post(url, json=first_data)  # регистрируем юзера
-
-    url = 'http://localhost:5000/login'
-    response = requests.post(url, json=JSON_input)
+def test_auth_negative(JSON_input, expected, user):
+    user({"login": "user_1", "password": "111", "admin": False, "expiration_date": '2019/11/30'})
+    get_user_login_url = 'http://localhost:5000/login'
+    response = requests.post(get_user_login_url, json=JSON_input)
     response = response.json()
-    with connect() as session:
-        operation_result = session.query(User).delete()
     assert response['error'] == expected
 
 
-def test_auth_positive():
-    url = 'http://localhost:5000/user'
-    first_data = {"login": "user_1", "password": "111", "admin": False, "expiration_date": '2019/12/31'}
-    requests.post(url, json=first_data)  # регистрируем юзера
-
-    url = 'http://localhost:5000/login'
+def test_auth_positive(user):
+    user({"login": "user_1", "password": "111", "admin": False, "expiration_date": '2019/12/31'})
+    get_user_login_url = 'http://localhost:5000/login'
     data = {'login': 'user_1', 'password': '111'}
-    response = requests.post(url, json=data)
+    response = requests.post(get_user_login_url, json=data)
     response = response.json()
-    with connect() as session:
-        operation_result = session.query(User).delete()
     assert bool(response['data']) is True
 
 
